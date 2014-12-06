cb8bc880cd5a58b59e72f692e092a137a0e3e2f9
MikeCAT <mikecat@users.noreply.github.com>
2014-12-07 08:42:34 +0900

    move some functions to scheme_modoki_core.cpp

--- scheme_modoki.h
+++ scheme_modoki.h
@@ -1,22 +1,9 @@
 #ifndef SCHEME_MODOKI_H_GUARD_66217EC2_CAC2_42C6_82A5_13D31B0E4EA4
 #define SCHEME_MODOKI_H_GUARD_66217EC2_CAC2_42C6_82A5_13D31B0E4EA4
 
-#include <string>
-#include <vector>
-#include "p_data.h"
-
 // 省略できるconsセルの表示を省略するかを設定する
 void set_do_syouryaku(bool enable);
 // 省略できるconsセルの表示を省略するかを取得する
 bool get_do_syouryaku();
-
-// 名前の規則を用いて名前を探し、そのポインタを返す
-p_data_t* namae_no_kisoku2(const std::string& namae,p_data_t& kankyo);
-// 名前の規則を用いて名前を探し、そのデータを返す
-p_data_t namae_no_kisoku(const std::string& namae,p_data_t& kankyo);
-// 適用の規則を用いて手続きを適用する
-p_data_t apply_proc(const p_data_t& proc, const std::vector<p_data_t>& args,p_data_t& kankyo);
-// 記号やリストで表現された式データを評価する
-p_data_t evaluate(const p_data_t& data,p_data_t& kankyo);
 
 #endif
---
+++ scheme_modoki_core.h
@@ -0,0 +1,20 @@
+#ifndef SCHEME_MODOKI_CORE_H_GUARD_89E598DB_24E2_42B4_BB04_ABDD8A9D91E2
+#define SCHEME_MODOKI_CORE_H_GUARD_89E598DB_24E2_42B4_BB04_ABDD8A9D91E2
+
+#include <string>
+#include <vector>
+#include "p_data.h"
+#include "stream_reader.h"
+
+// 名前の規則を用いて名前を探し、そのポインタを返す
+p_data_t* namae_no_kisoku2(const std::string& namae,p_data_t& kankyo);
+// 名前の規則を用いて名前を探し、そのデータを返す
+p_data_t namae_no_kisoku(const std::string& namae,p_data_t& kankyo);
+// 適用の規則を用いて手続きを適用する
+p_data_t apply_proc(const p_data_t& proc, const std::vector<p_data_t>& args,p_data_t& kankyo);
+// 記号やリストで表現された式データを評価する
+p_data_t evaluate(const p_data_t& data,p_data_t& kankyo);
+// 入力をパースしてquoteされた状態のリストを得る
+p_data_t parse(stream_reader& sr);
+
+#endif
