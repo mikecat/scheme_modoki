843ebe2286ee88ee322fc74f1fea8b9fc7d00cdc
MikeCAT <mikecat@users.noreply.github.com>
2014-12-07 10:37:11 +0900

    added get_name() to data_t

--- data.h
+++ data.h
@@ -36,39 +36,48 @@ struct data_t {
 
 	// このデータの種類を返す
 	virtual DATATYPE get_type() const = 0;
+	// このデータの名前を返す
+	virtual const char* get_name() const = 0;
 };
 
 struct eof_t : public data_t {
 	DATATYPE get_type() const {return DT_EOF;}
+	const char* get_name() const {return "EOF";}
 };
 
 struct exit_t: public data_t {
 	DATATYPE get_type() const {return DT_EXIT;}
+	const char* get_name() const {return "exit";}
 	int exit_code;
 };
 
 struct error_t : public data_t {
 	DATATYPE get_type() const {return DT_ERROR;}
+	const char* get_name() const {return "error";}
 	std::string error_mes;
 };
 
 struct num_t : public data_t {
 	DATATYPE get_type() const {return DT_NUM;}
+	const char* get_name() const {return "number";}
 	double num;
 };
 
 struct kigou_t : public data_t {
 	DATATYPE get_type() const {return DT_KIGOU;}
+	const char* get_name() const {return "kigou";}
 	std::string kigou;
 };
 
 struct boolean_t : public data_t {
 	DATATYPE get_type() const {return DT_BOOLEAN;}
+	const char* get_name() const {return "boolean";}
 	bool is_true;
 };
 
 struct lambda_t : public data_t {
 	DATATYPE get_type() const {return DT_LAMBDA;}
+	const char* get_name() const {return "lambda-siki";}
 	std::vector<std::string> karihikisu;
 	std::vector<p_data_t> hontai;
 	p_data_t lambda_kankyo;
@@ -77,22 +86,26 @@ struct lambda_t : public data_t {
 
 struct cons_t : public data_t {
 	DATATYPE get_type() const {return DT_CONS;}
+	const char* get_name() const {return "cons-cell";}
 	p_data_t cons_car;
 	p_data_t cons_cdr;
 };
 
 struct native_func_t : public data_t {
 	DATATYPE get_type() const {return DT_NATIVE_FUNC;}
+	const char* get_name() const {return "native func";}
 	p_native_func native_func;
 	bool tokusyu_keisiki;
 };
 
 struct null_t : public data_t {
 	DATATYPE get_type() const {return DT_NULL;}
+	const char* get_name() const {return "null";}
 };
 
 struct kankyo_t : public data_t {
 	DATATYPE get_type() const {return DT_KANKYO;}
+	const char* get_name() const {return "kankyo";}
 	p_data_t parent;
 	std::map<std::string, p_data_t> sokubaku;
 };
