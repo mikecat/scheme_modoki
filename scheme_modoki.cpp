e09532bae3f95938da64273e926627cdab1c6a4f
MikeCAT <mikecat@users.noreply.github.com>
2014-12-07 09:02:45 +0900

    make run_script() separate from main()

--- scheme_modoki.cpp
+++ scheme_modoki.cpp
@@ -5,6 +5,7 @@
 #include "kumikomi_tetuduki.h"
 
 static bool do_syouryaku=true;
+static p_data_t taiiki_kankyo;
 
 void set_do_syouryaku(bool enable) {
 	do_syouryaku=enable;
@@ -14,7 +15,29 @@ bool get_do_syouryaku() {
 	return do_syouryaku;
 }
 
+int run_script(stream_reader& sr,bool is_interactive) {
+	int exit_code=0;
+	for(;;) {
+		p_data_t data;
+		if(is_interactive)printf("input> ");
+		data=parse(sr);
+		if(data->get_type()==DT_EOF)break;
+		data=evaluate(data,taiiki_kankyo);
+		if(data->get_type()==DT_EOF)break;
+		else if(data->get_type()==DT_EXIT) {
+			exit_code=((exit_t*)&*data)->exit_code;
+			break;
+		}
+		if(is_interactive) {
+			print_data(data,do_syouryaku);
+			putchar('\n');
+		}
+	}
+	return exit_code;
+}
+
 int main(int argc,char *argv[]) {
+	// コマンドライン引数を読み込む
 	for(int i=1;i<argc;i++) {
 		std::string cur_arg=argv[i];
 		if(cur_arg=="--no-syouryaku") {
@@ -23,23 +46,14 @@ int main(int argc,char *argv[]) {
 			p_data_config_t::set_do_auto_delete(false);
 		}
 	}
-	int exit_code=0;
-	p_data_t taiiki_kankyo=creater_t::creater().create_kankyo_data();
+	// 大域環境を作成する
+	taiiki_kankyo=creater_t::creater().create_kankyo_data();
 	add_kumikomi_tetuduki_to_kankyo(taiiki_kankyo);
+	// スクリプトファイルを開く
 	file_reader fr(stdin);
-	for(;;) {
-		p_data_t data;
-		printf("input> ");
-		data=parse(fr);
-		if(data->get_type()==DT_EOF)break;
-		data=evaluate(data,taiiki_kankyo);
-		if(data->get_type()==DT_EOF)break;
-		else if(data->get_type()==DT_EXIT) {
-			exit_code=((exit_t*)&*data)->exit_code;
-			break;
-		}
-		print_data(data,do_syouryaku);
-		putchar('\n');
-	}
-	return exit_code;
+	// スクリプトを実行
+	int ret=run_script(fr,true);
+	// 大域環境を開放させる(自動削除が有効の場合)
+	taiiki_kankyo=NULL;
+	return ret;
 }
--- scheme_modoki.h
+++ scheme_modoki.h
@@ -1,9 +1,14 @@
 #ifndef SCHEME_MODOKI_H_GUARD_66217EC2_CAC2_42C6_82A5_13D31B0E4EA4
 #define SCHEME_MODOKI_H_GUARD_66217EC2_CAC2_42C6_82A5_13D31B0E4EA4
 
+#include "stream_reader.h"
+
 // 省略できるconsセルの表示を省略するかを設定する
 void set_do_syouryaku(bool enable);
 // 省略できるconsセルの表示を省略するかを取得する
 bool get_do_syouryaku();
+
+// 大域環境でスクリプトを実行する
+int run_script(stream_reader& sr,bool is_interactive);
 
 #endif
