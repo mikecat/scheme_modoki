e97449883b8834bb9ccedc9fed09ed3053ebd767
MikeCAT <mikecat@users.noreply.github.com>
2014-12-07 10:55:07 +0900

    improved comments

--- kumikomi_tetuduki/system_control.h
+++ kumikomi_tetuduki/system_control.h
@@ -8,13 +8,13 @@
 p_data_t set_enable_syouryaku(const std::vector<p_data_t>& args,p_data_t&);
 // 省略できるconsセルの表示を省略するかを取得する
 p_data_t get_enable_syouryaku(const std::vector<p_data_t>& args,p_data_t&);
-// 生成され、削除されていない環境とデータの数を表示する
+// 生成され、削除されていないデータの数を表示する
 p_data_t print_statistics(const std::vector<p_data_t>& args,p_data_t&);
-// 環境とデータの自動削除の有効/無効を設定する
+// データの自動削除の有効/無効を設定する
 p_data_t set_enable_auto_delete(const std::vector<p_data_t>& args,p_data_t&);
-// 環境とデータの自動削除の有効/無効を取得する
+// データの自動削除の有効/無効を取得する
 p_data_t get_enable_auto_delete(const std::vector<p_data_t>& args,p_data_t&);
-// 参照カウントが0以下の環境とデータを削除する
+// 参照カウントが0以下のデータを削除する
 p_data_t delete_zero_sansyo(const std::vector<p_data_t>& args,p_data_t&) ;
 
 #endif
